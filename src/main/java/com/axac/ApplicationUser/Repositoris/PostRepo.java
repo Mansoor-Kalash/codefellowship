bc5e86cf3bbee52907392f2cf6442f4a0d0f8552
Mansoor-kalash <85104388+Mansoor-Kalash@users.noreply.github.com>
2021-11-02 01:39:03 +0200

    Revert "lab 16-17"

--- src/main/java/com/axac/ApplicationUser/Repositoris/PostRepo.java
+++
@@ -1,8 +0,0 @@
-package com.axac.ApplicationUser.Repositoris;
-
-import com.axac.ApplicationUser.Models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepo extends CrudRepository<Post, Long> {
-
-}
--- src/main/java/com/axac/ApplicationUser/Repositoris/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.axac.ApplicationUser.Repositoris;
-
-
-import com.axac.ApplicationUser.Models.ApplicationUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<ApplicationUser,Long> {
-    ApplicationUser findByUsername(String username);
-}
